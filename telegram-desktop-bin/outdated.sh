e5d1cdd70088c3bf9c0de2498159cfaac8dd5884
Vardan Gevorgyan <vgevorgyan@vtgsoftware.com>
2025-10-15 15:56:29 +0400

    Added telegram-desktop-bin package.

---
+++ telegram-desktop-bin/outdated.sh
@@ -0,0 +1,11 @@
+#!/bin/env sh
+
+set -eu
+
+script_dir="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+. "$script_dir/../lib.sh"
+
+check_github_latest_release "$script_dir/PKGBUILD" "telegramdesktop" "tdesktop"
+
+exit 0
+
